24ac810d9df47fc94e3a80b7ef6c07bd7e842922
Ellery Temple Jr <elreyatee@gmail.com>
2014-04-22 08:21:45 -0700

    Finished
    Fixed jQuery, changed styling a bit, tested operation

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
-$(document).ready(function(){
-	$("#shopping-field").on('click', '.click-basket', function(event) {
-		
+$(document).ready(function() {
+
+	function playPapersound () {
+		$('#bag-sound')[0].volume = 0.5;
+		$('#bag-sound')[0].load();
+		$('#bag-sound')[0].play();
+	}
+
+	function clearField () {
+		$('#input-field').val('');
+	}
+
+	function focusInput () {
+		$('#input-field').focus();
+	}
+
+	focusInput();
+
+	$('#clear-field').on('click', function() {
+		clearField();
 	});
 
+	$('#add-item').on('click', function() {
+		var item = $('#input-field').val();
+
+		if(item !== '') {
+			playPapersound();
+			$('<div><p>' + item + '</p><a href="#" id="clear-field" class="basket-controls"></a></div>')
+				.appendTo('#shopping-bag')
+				.addClass('bag-item');
+			clearField();
+			focusInput();
+		} else {
+			alert('Please enter an item');
+		}
+		$('.bag-item a').on('click', function() {
+			$(this).closest('.bag-item').remove();
+			focusInput();
+		});
+	});
+
+	
 });
