5ed1b7724945ff4e01a07b27ba7ca28f6a84bec2
Ellery Temple Jr <elreyatee@gmail.com>
2014-06-11 04:44:28 -0700

    fixed cross domain issues

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
+var longitude, latitude;
+
 $(document).ready(function() {
 
 	function playPapersound () {
 		$('#bag-sound')[0].volume = 0.5;
 		$('#bag-sound')[0].load();
 		$('#bag-sound')[0].play();
+	}
+
+	// Epoch unix timestamp
+	var ts = Math.round((new Date()).getTime() / 1000);
+	console.log(ts);
+
+	// Checks to see if geolocation in browser is enabled
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else {
+		alert("Geolocation is not supported by your browser!");
+	}
+
+	function showPosition(position) {
+		console.log("Latitude: " + position.coords.latitude + " & Longitude: " + position.coords.longitude);
+		longitude = position.coords.longitude;
+		latitude = position.coords.latitude;
+
+		var randoms = Math.random().toString(36).slice(2);
+
+		var yelpResult = $.ajax({
+			url: 'http://api.yelp.com/v2/search?term=grocery&cll=' +
+						latitude + ',' + longitude,
+			type: 'POST',
+			jsonp: 'callback',
+			dataType: 'jsonp',
+			headers: {oauth_consumer_key: 'kV4eJDAd5AFOzGyMYhQpyQ',
+					  oauth_consumer_secret: 'olcIRixTohwU94qouGwV4VEcn9E',
+					  oauth_token: 'zM2_GgTAJTV1KwIwp7iC1FPBtObaTYKw',
+					  oauth_signature_method: 'HMAC-SHA1',
+					  oauth_signature: '3059b14cc0155f2f489b8564ad4888da5371eb21',
+					  oauth_timestamp: ts.toString(),
+					  oauth_nonce: randoms}
+		}).done(function(data) {
+			console.log('Got it!!');
+		});
 	}
 
 	function clearField () {
@@ -20,7 +59,8 @@ $(document).ready(function() {
 		clearField();
 	});
 
-	$('#add-item').on('click', function() {
+	$('#add-item').on('click', function(event) {
+		event.preventDefault();
 		var item = $('#input-field').val();
 
 		if(item !== '') {
---
+++ application.js
@@ -0,0 +1,96 @@
+window.onload = function() {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else {
+		alert("Geolocation is not supported by your browser!");
+	}
+};
+
+var latlon;
+
+var showPosition = function (position) {
+	console.log("Latitude: " + position.coords.latitude + " & Longitude: " + position.coords.longitude);
+	latlon = position.coords.latitude + ',' + position.coords.longitude;	
+};
+
+var playPapersound = function () {
+	$('#bag-sound')[0].volume = 0.5;
+	$('#bag-sound')[0].load();
+	$('#bag-sound')[0].play();
+};
+
+var clearField = function () {
+	$('#input-field').val('');
+};
+
+var focusInput = function () {
+	$('#input-field').focus();
+};
+
+//Init
+var oauth = OAuth({
+	consumer: {
+		public: 'kV4eJDAd5AFOzGyMYhQpyQ',
+		secret: 'olcIRixTohwU94qouGwV4VEcn9E'
+	},
+	signature_method: 'HMAC-SHA1'
+});
+
+// Request Data
+var request_data = {
+	url: 'http://api.yelp.com/v2/search?term=grocery&limit=5&sort=2&ll='+latlon,
+	method: 'POST',
+	data: {
+		status: 'Hello!'
+	}
+};
+
+// Token info
+var token = {
+	public: 'zM2_GgTAJTV1KwIwp7iC1FPBtObaTYKw',
+	secret: 'K9AQEMBhDeC-ZeYY--QvZAprk0o'
+};
+
+$(document).ready(function() {
+
+	$('#menu-bar').on('click', 'li a img[alt="Yelp"]', function(event) {
+		event.preventDefault();
+		var yelpResult = $.ajax({
+				url: request_data.url,
+				jsonp: 'callback',
+				dataType: 'jsonp',
+				type: request_data.method,
+				data: oauth.authorize(request_data, token)
+			}).done(function(data) {
+				console.log('success!');
+		});
+	});
+
+	focusInput();
+
+	$('#clear-field').on('click', function() {
+		clearField();
+	});
+
+	$('#add-item').on('click', function(event) {
+		event.preventDefault();
+		var item = $('#input-field').val();
+
+		if(item !== '') {
+			playPapersound();
+			$('<div><p>' + item + '</p><a href="#" id="clear-field" class="basket-controls"></a></div>')
+				.appendTo('#shopping-bag')
+				.addClass('bag-item');
+			clearField();
+			focusInput();
+		} else {
+			alert('Please enter an item');
+		}
+		$('.bag-item a').on('click', function() {
+			$(this).closest('.bag-item').remove();
+			focusInput();
+		});
+	});
+
+});
